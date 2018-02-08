c722ca9fc06adb64f26e148ab6244272ddfcbf32
Bing Cui <bcui@Bings-MBP.fios-router.home>
2018-02-07 23:55:21 -0500

    big table test1

--- main.go
+++ main.go
@@ -94,11 +94,6 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	id := uuid.New()
 	saveToES(&p, id)
 	// Save to BigTable
-	saveToBigTable(&p)
-}
-
-// Save posts to BigTable
-func saveToBigTable(p *Post) {
 	ctx := context.Background()
 	// you must update project name here
 	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
@@ -107,20 +102,22 @@ func saveToBigTable(p *Post) {
 		return
 	}
 
-	tbl := bt_client.Open("mytable")
+	tbl := bt_client.Open("post")
 	mut := bigtable.NewMutation()
 	t := bigtable.Now()
+
 	mut.Set("post", "user", t, []byte(p.User))
 	mut.Set("post", "message", t, []byte(p.Message))
 	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
 	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
 
-	errCreateTable := tbl.Apply(ctx, "com.google.cloud", mut)
-	if errCreateTable != nil {
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
 		panic(err)
 		return
 	}
 	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+
 }
 
 // Save a post to ElasticSearch
